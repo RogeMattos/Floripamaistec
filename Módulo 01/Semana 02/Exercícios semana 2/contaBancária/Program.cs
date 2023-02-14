3e712240e4cdf3ed02c8c37f51a1ed894ec9c8d1
RogeMattos <rogemattos.rm@gmail.com>
2023-02-14 08:41:50 -0300

    Semana 2 - exercicio 3/4/5/6

---
+++ Módulo 01/Semana 02/Exercícios semana 2/contaBancária/Program.cs
@@ -0,0 +1,10 @@
+﻿using contaBancária.contaBancaria;
+
+contaBancaria conta = new contaBancaria();
+
+conta.numeroConta = 5248;
+conta.agencia = 1234;
+conta.Titular = "Rogerio Mattos";
+
+Console.WriteLine($"O número da conta é: {conta.numeroConta} O numero da agência é: {conta.agencia} O nome do Titular é: {conta.Titular}");
+
