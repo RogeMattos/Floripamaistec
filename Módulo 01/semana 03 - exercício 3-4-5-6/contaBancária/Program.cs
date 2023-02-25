819e5babcf45c21c880ea8846f22ea5dfb732999
RogeMattos <rogemattos.rm@gmail.com>
2023-02-24 21:39:35 -0300

    Semana 03 - exercicio 3-4-5-6

---
+++ Módulo 01/semana 03 - exercício 3-4-5-6/contaBancária/Program.cs
@@ -0,0 +1,22 @@
+﻿using contaBancária;
+using contaBancária.contaBancaria;
+
+
+
+
+ContaBancaria conta1 = new ContaBancaria(1542, 1234, "João Silva", TipoContaEnum.POUPANCA);
+ContaBancaria conta2 = new ContaBancaria(1545, 1234, "João Silva", TipoContaEnum.POUPANCA);
+
+// testando metodo depositar
+conta1.Depositar(200);
+
+conta1.Transferir(conta2, 50);
+
+// exibe o saldo da conta1
+conta1.ExibirSaldo();
+// deve retornar que o saldo é 150
+
+// exibe o saldo da conta1
+conta2.ExibirSaldo();
+// deve retornar que o saldo é 50
+
