e20c6e397650ffe3001b35ae85e5787dbcfdc483
RogeMattos <rogemattos.rm@gmail.com>
2023-03-01 12:35:45 -0300

    Semana 05 - exercicio 2

---
+++ Módulo 01/Semana 05 - exercício 2/Program.cs
@@ -0,0 +1,29 @@
+﻿//zona segura
+FileInfo arquivo = null;
+try
+{
+    Console.Write("Digite o nome do arquivo que voce vai criar: ");
+    String nomeArquivo = Console.ReadLine();
+
+    arquivo = new FileInfo(nomeArquivo);
+
+    using (StreamWriter gravadorFluxo = File.AppendText(nomeArquivo))
+    {
+        gravadorFluxo.WriteLine("Texto de dentro do arquivo");
+    }
+}
+
+    catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+	
+}
+finally
+{
+    Console.WriteLine($"Encerrando o programa...\n");
+   // arquivo = null;
+    //Console.Clear();
+    
+}
+
+
