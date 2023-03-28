f889ca0baa9fc22859a89ab88e8d1546291d58bc
RogeMattos <rogemattos.rm@gmail.com>
2023-03-28 16:13:57 -0300

    Semana 08 - exercício 1 a 11

---
+++ Módulo 01/Semana 08 - exercício 1 a 11/Escola/Dtos/CriacaoAlunoDto.cs
@@ -0,0 +1,12 @@
+
+namespace Escola.Dtos;
+
+public class AlunoDto 
+{
+    public int id { get; set; }
+
+    public string? Nome{ get; set; }
+
+    public DateTime DataDeNascimento { get; set; }
+    
+}
---
+++ Módulo 01/Semana 08 - exercício 1 a 11/Escola/Models/BaseModel.cs
@@ -0,0 +1,11 @@
+namespace Escola.Models;
+
+public class BaseModel 
+{
+
+    public int id { get; set; }
+
+    public DateTime DataDeInclusao { get; set; }
+
+    public DateTime DataDeAlterção { get; set; }
+}
