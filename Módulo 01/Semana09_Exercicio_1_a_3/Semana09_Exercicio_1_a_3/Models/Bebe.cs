510dc5a465c61c6937f8302f339d19c39c2aebf3
RogeMattos <rogemattos.rm@gmail.com>
2023-04-24 15:46:49 -0300

    Semana 09 - exercicio 1 a 3

---
+++ Módulo 01/Semana09_Exercicio_1_a_3/Semana09_Exercicio_1_a_3/Models/Bebe.cs
@@ -0,0 +1,21 @@
+﻿namespace Semana09_Exercicio_1_a_3.Models
+{
+    public class Bebe
+    {
+        public int Id { get; set; }
+
+        public DateTime Data_Nascimento { get; set; }
+
+        public decimal Peso_Nascimento { get; set; }
+
+        public int Altura { get; set; }
+
+        public int Id_Mae { get; set; }
+
+        public int Id_Parto { get; set; }
+
+        public virtual Mae Mae { get; set; }
+        public virtual Parto Parto { get; set; }
+
+    }
+}
---
+++ Módulo 01/Semana09_Exercicio_1_a_3/Semana09_Exercicio_1_a_3/Models/Configurations/BebeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Semana09_Exercicio_1_a_3.Models.Configurations
+{
+    public class BebeConfiguration : IEntityTypeConfiguration<Bebe>
+    {
+        public void Configure(EntityTypeBuilder<Bebe> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            // 1 para muitos
+            builder.HasOne(x => x.Mae) // 1 
+                    .WithMany(p => p.Bebes) // muitos
+                    .HasForeignKey(x => x.Id_Mae) // FK representante
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Bebe_Mae");
+
+
+
+            // 1 para muitos
+            builder.HasOne(x => x.Parto) // 1 
+                    .WithMany(p => p.Bebes) // muitos
+                    .HasForeignKey(x => x.Id_Parto) // FK representante
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Bebe_Parto");
+
+            builder.ToTable("Bebe");
+        }
+    }
+}
