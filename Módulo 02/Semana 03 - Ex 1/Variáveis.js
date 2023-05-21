1f15ac60b16b51675fcfff4d76f483d3fc27d0b2
RogeMattos <rogemattos.rm@gmail.com>
2023-05-21 17:12:27 -0300

    Semana 03 - Ex 1

---
+++ Módulo 02/Semana 03 - Ex 1/Variáveis.js
@@ -0,0 +1,29 @@
+Uma variável é um espaço na memória do computador destinado a um dado que é alterado durante a execução do algoritmo.
+
+Declaração de  uma varável:
+var sobrenome = “Mattos”
+
+String: uma string é um conjunto e caracteres delimitados por aspas simples ou aspas duplas:
+var nome = “Rogério”
+
+Number:  um number representa números (inteiros  ou flutuantes)
+var idade = 50;
+var altura = 1.75;
+
+Boolean: Representa um valor lógico verdadeiro ou falso
+var ativo = true;
+var menor deidade = false;
+
+Object: Representa um objeto, que é uma coleção de propriedades
+var pessoa = {
+nome: “Fábio”,
+idade: 25,
+cidade: “Porto Alegre”
+}
+
+Array: Representa uma lista de valores
+var numeros = [ 1,3,5,6,8];
+
+Null: Representa a ausência intencional de valor
+var salarioMensal = null;
+
