192fccaae61923fa6f99148de7873cc11dc1a49b
RogeMattos <rogemattos.rm@gmail.com>
2023-05-28 19:26:17 -0300

    Semana 04 - Ex 3

---
+++ Módulo 02/Semana 04 - Ex 3/script.js
@@ -0,0 +1,11 @@
+
+function soma (){
+
+    const numero1 = parseInt(document.getElementById('numero1').value) 
+    const numero2 = parseInt(document.getElementById('numero2').value) 
+
+    const resultado = numero1 + numero2
+
+    document.getElementById('Resultado').innerHTML = resultado
+
+}
