6a565d6813f705f0e46859c7a2427d7de81fcdd0
RogeMattos <rogemattos.rm@gmail.com>
2023-06-20 22:04:27 -0300

    Semana 06 - Ex 4

---
+++ Módulo 02/Semana 06 - Ex 4/script.js
@@ -0,0 +1,33 @@
+
+export function buscaLista(element) {
+    
+    fetch('https://servicodados.ibge.gov.br/api/v1/censos/nomes/ranking')
+        .then(response => response.json())
+        .then(data => {
+            // Inverte a lista
+            let listaInvertida = data.reverse();
+            const listaFiltrada = listaInvertida.filter(item => item.nome.toLowerCase().includes('a'));
+            // Itera sobre a lista invertida e adiciona os itens ao elemento HTML
+            listaFiltrada.forEach(item => {
+                const div = document.createElement('div');
+                div.textContent = `Nome: ${item.nome}, Rank: ${item.rank}`;
+                element.append(div)
+            });
+
+
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+
+}
+
+
+
+
+
+
+
+
+
