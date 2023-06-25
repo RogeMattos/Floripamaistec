0eda00d779e01a74fe3a20703fa459bdb2432771
RogeMattos <rogemattos.rm@gmail.com>
2023-06-25 10:35:44 -0300

    Semana 05 - Ex_5

---
+++ Módulo 02/Semana 05 - EX_5/home.js
@@ -0,0 +1,62 @@
+const lista = [
+  {
+    titulo: "Floresta Brasileira",
+    imagem:
+      "https://i2.wp.com/files.agro20.com.br/uploads/2019/07/floresta-amazonica-2.jpg?resize=600%2C338&ssl=1",
+    subtitulo:
+      "Florestas sendo devastadas",
+  },
+  {
+    titulo: "Fauna Brasileira",
+    imagem:
+      "https://www.infoescola.com/wp-content/uploads/2008/05/onca-pintada-591459416.jpg",
+    subtitulo:
+      "Animais em extinção",
+  },
+  {
+    titulo: "Crise Econômica",
+    imagem:
+      "https://s2.glbimg.com/IXrw7WfWnbXI5NqqQlXeyoFAEk0=/403x136:2106x1108/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_f035dd6fd91c438fa04ab718d608bbaa/internal_photos/bs/2020/z/c/8zFznaQIiXvkZ5XAH6Hw/lupa.jpg",
+    subtitulo: "Déficiti de investimentos",
+  },
+  {
+    titulo: "Educação",
+    imagem:
+      "https://img.freepik.com/fotos-premium/ilustracao-2d-da-sala-de-aula-a-noite_141465-61.jpg",
+    subtitulo:
+      "Crise na Educação do Brsail",
+  },
+  {
+    titulo: "Polítca",
+    imagem:
+      "https://www.tse.jus.br/imagens/fotos/voto-eletronico-diminui-votos-invalidos-em-09-08.2022-871827/@@images/6eb11c3a-ca78-4b58-9c5c-1642f37c3ad3.jpeg",
+    subtitulo:
+      "Será que houve fraude nas últimas eleições?",
+  },
+  {
+    titulo: "Agricultura",
+    imagem:
+      "https://terramagna.com.br/wp-content/uploads/2022/07/Colheitadeira-trigo-lavoura.jpg",
+    subtitulo: "Reforma agrária",
+  },
+  {
+    titulo: "Música Brasileira",
+    imagem:
+      "https://1.bp.blogspot.com/-CaF5tpMzwsQ/Xai2lVNDiEI/AAAAAAAAfy0/QgOU_yx-bQ0vLujllX-l1TTnw2mYvPh6ACLcBGAsYHQ/s1600/MPB.jpg",
+    subtitulo: "Música brasileira no top mundial?",
+  },
+  {
+    titulo: "carros Importados",
+    imagem:
+      "https://1.bp.blogspot.com/-ix_6eVVPZqs/XUGK_rNFsII/AAAAAAAAB7g/Yupooow4EH8-6avY3yx0felFsKLyO0f7wCLcBGAs/s1600/Volvo-XC60-2020-China%2B%25282%2529.jpg",
+    subtitulo:
+      "carros importados vão substituir os nacionais??",
+  },
+  {
+    titulo: "Literatura",
+    imagem:
+      "https://www.intersaberes.com/wp-content/uploads/2022/05/AdobeStock_215842088-1-1024x576.jpeg",
+    subtitulo:
+      "Porque os Brasileiros não tem o hpábitoda leitura?",
+  },
+];
