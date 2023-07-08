7dfee345cd9da7cf77bcc12ec415d933234cba36
RogeMattos <rogemattos.rm@gmail.com>
2023-07-08 11:45:21 -0300

    Semana 08 - Ex_09

---
+++ Módulo 02/Semana 08 - Ex_09/src/app/components/notification-list/notification-list.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-list',
+  templateUrl: './notification-list.component.html',
+  styleUrls: ['./notification-list.component.scss']
+})
+export class NotificationListComponent {
+
+  notificationList: any[] = [
+    {
+      title: 'Nova mensagem',
+      from: 'João Silva',
+      message: 'Você recebeu uma nova mensagem de João Silva.',
+      date: '25 de Junho, 2023',
+      lido: false,
+      buttonText: 'Marcar como Lido'
+    },
+    {
+      title: 'Atualização do sistema',
+      from: 'Admin',
+      message: 'Uma nova atualização do sistema está disponível. Por favor, atualize para a versão mais recente.',
+      date: '24 de Junho, 2023',
+      lido: false,
+      buttonText: 'Marcar como Lido'
+    },
+    {
+      title: 'Lembrete de reunião',
+      from: 'Maria Souza',
+      message: 'Lembrete: Reunião de equipe às 14h. Não se esqueça de participar.',
+      date: '23 de Junho, 2023',
+      lido: false,
+      buttonText: 'Marcar como Lido'
+    }
+  ];
+
+  marcarComoLido(notification: any) {
+    notification.lido = true;
+    notification.buttonText = 'Lido';
+  }
+}
+
+
