33b64672e863731ee4969dc834c2f207a4393377
RogeMattos <rogemattos.rm@gmail.com>
2023-07-22 16:14:50 -0300

    Semana 08 - Ex-07

---
+++ Módulo 02/Semana 08 - Ex-07/src/app/components/notification-list/notification-list.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-list',
+  templateUrl: './notification-list.component.html',
+  styleUrls: ['./notification-list.component.scss']
+})
+export class NotificationListComponent {
+
+  notification_1: any = {
+    title: 'Nova mensagem',
+    from: 'João Silva',
+    message: 'Você recebeu uma nova mensagem de João Silva.',
+    date: '25 de Junho, 2023'
+  };
+
+  notification_2: any = {
+    title: 'Atualização do Sistema',
+    from: 'Admin',
+    message: 'Uma nova atualização do sistema está disponível. Por favor, atualize para a versão mais recente.',
+    date: '24 de Junho, 2023'
+  };
+
+  notification_3: any = {
+    title: 'Lembrete de reunião',
+    from: 'Maria Souza',
+    message: 'Lembrete: Reunião de equipe às 14h. Não se esqueça de participar.',
+    date: '23 de Junho, 2023'
+  };
+}
+
+
