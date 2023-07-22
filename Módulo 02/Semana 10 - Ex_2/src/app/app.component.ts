1df1b3d4117bab94fac26d309fd993e7d1aeb690
RogeMattos <rogemattos.rm@gmail.com>
2023-07-22 15:39:14 -0300

    Semana 10 - Ex_2

---
+++ Módulo 02/Semana 10 - Ex_2/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nfd-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-food_Ex_2';
+}
