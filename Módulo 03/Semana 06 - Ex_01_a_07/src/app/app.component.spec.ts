c99564bf776f08848b15240305e77240b69b2e93
RogeMattos <rogemattos.rm@gmail.com>
2023-09-20 16:59:12 -0300

    Semana 06 - Ex_01_a_07

---
+++ Módulo 03/Semana 06 - Ex_01_a_07/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { ExerciciosComponent } from './components/exercicios/exercicios.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent,ExerciciosComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'M03S06-Exercicios1_a_7'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('M03S06-Exercicios1_a_7');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('M03S06-Exercicios1_a_7 app is running!');
+  });
+  
+});
---
+++ Módulo 03/Semana 06 - Ex_01_a_07/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ExerciciosComponent } from './components/exercicios/exercicios.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExerciciosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
